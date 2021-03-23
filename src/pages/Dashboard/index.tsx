3fb2a02c11d9ebf44f86b1f902b5db812bb651ba
Vanessa Santana <vanessah4money@gmail.com>
2021-03-23 13:32:07 -0300

    feat: get data from Github Api

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,35 +1,52 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 import logoImg from '../../assets/logo.svg';
 import { Title, Form, Repositories } from './styles';
+import api from '../../services/api';
 
-const Dashboard: React.FC = () => (
-  <>
-    <img src={logoImg} alt="Github Explorer" />
-    <Title>Explore repositórios no GitHub!</Title>
-    <Form>
-      <input placeholder="Digite o nome do repositório" />
-      <button type="submit">Pesquisar</button>
-    </Form>
-    <Repositories>
-      <a href="teste">
-        <img src="https://avatars.githubusercontent.com/u/52542645?s=460&u=17b4727e3f0ce3d7354ea7957d8e9a5183d04b2c&v=4" alt="Vanessa Santana" />
-        <div>
-          <strong>Repositório</strong>
-          <p>Descrição do repositório</p>
-        </div>
-        <FiChevronRight size={20} />
-      </a>
-      <a href="teste">
-        <img src="https://avatars.githubusercontent.com/u/52542645?s=460&u=17b4727e3f0ce3d7354ea7957d8e9a5183d04b2c&v=4" alt="Vanessa Santana" />
-        <div>
-          <strong>Repositório</strong>
-          <p>Descrição do repositório</p>
-        </div>
-        <FiChevronRight size={20} />
-      </a>
-    </Repositories>
-  </>
-);
+interface Repository{
+    'full_name': string;
+    description: string;
+    owner: {
+        login: string;
+        'avatar_url': string;
+    }
+}
+
+const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+  async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
+    event.preventDefault();
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no GitHub!</Title>
+      <Form onSubmit={handleAddRepository}>
+        <input value={newRepo} onChange={(e) => setNewRepo(e.target.value)} placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+      <Repositories>
+        {repositories.map((repository) => (
+          <a key={repository.full_name} href="teste">
+            <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
+      </Repositories>
+    </>
+  );
+};
 
 export default Dashboard;
